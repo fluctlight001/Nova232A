6ef76ec33ae30fee03d375952d3ab38319dce573
fluctlight001 <fluctlight@qq.com>
2022-05-11 00:56:17 +0800

    update bpu

--- axi4_v2/bpu.v
+++ axi4_v2/bpu.v
@@ -12,39 +12,65 @@ module bpu(
     output wire next_inst_invalid,
     output wire [`BR_WD-1:0] bp_bus
 );
-    wire hit_way1;
-    wire hit_way2;
+    wire [1:0] hit_way, hit_way1, hit_way2;
     wire br_e;
     wire [31:0] br_target;
     wire bp_e;
     wire [31:0] bp_target;
-    reg valid;
-    reg [31:0] branch_history_pc;
-    reg [31:0] branch_target;
+    reg [1:0] valid;
+    reg [31:0] branch_history_pc [1:0];
+    reg [31:0] branch_target[1:0];
     
     assign {br_e, br_target} = br_bus;
     assign bp_bus = {bp_e, bp_target};
 
+    reg lru;
+
     always @ (posedge clk) begin
         if (!resetn) begin
-            valid <= 1'b0;
-            branch_history_pc <= 32'b0;
-            branch_target <= 32'b0;
+            lru <= 1'b0;
         end
-        else if (br_e) begin
-            valid <= 1'b1;
-            branch_history_pc <= delayslot_pc;
-            branch_target <= br_target;
+        else if (hit_way[0]) begin
+            lru <= 1'b1;
+        end
+        else if (hit_way[1]) begin
+            lru <= 1'b0;
         end
     end
 
-    assign hit_way1 = valid & (branch_history_pc == current_pc1);
-    assign hit_way2 = valid & (branch_history_pc == current_pc2);
+    always @ (posedge clk) begin
+        if (!resetn) begin
+            valid <= 1'b0;
+            branch_history_pc[0] <= 32'b0;
+            branch_history_pc[1] <= 32'b0;
+            branch_target[0] <= 32'b0;
+            branch_target[1] <= 32'b0;
+        end
+        else if (br_e & ~lru) begin
+            valid[0] <= 1'b1;
+            branch_history_pc[0] <= delayslot_pc;
+            branch_target[0] <= br_target;
+        end
+        else if (br_e & lru) begin
+            valid[1] <= 1'b1;
+            branch_history_pc[1] <= delayslot_pc;
+            branch_target[1] <= br_target;
+        end
+    end
 
-    assign next_inst_invalid = hit_way1;
+    assign hit_way1[0] = valid[0] & (branch_history_pc[0] == current_pc1);
+    assign hit_way1[1] = valid[1] & (branch_history_pc[1] == current_pc1);
+    assign hit_way2[0] = valid[0] & (branch_history_pc[0] == current_pc2);
+    assign hit_way2[1] = valid[1] & (branch_history_pc[1] == current_pc2);
 
-    assign bp_e = hit_way1 | hit_way2;
-    assign bp_target = branch_target;
+    assign hit_way[0] = hit_way1[0] | hit_way2[0];
+    assign hit_way[1] = hit_way1[1] | hit_way2[1];
+
+    assign next_inst_invalid = hit_way1[0] | hit_way1[1];
+
+    assign bp_e = hit_way[0] | hit_way[1];
+    assign bp_target = hit_way[0] ? branch_target[0] 
+                    : hit_way[1] ? branch_target[1] : 32'b0;
 
 
 
